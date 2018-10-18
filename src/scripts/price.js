af8a04b7d242a565897bed5ffd033bad613d66dd
Alexey Gusev <alexeigusev@yahoo.no>
2018-10-18 23:50:56 +0200

    price

---
+++ src/scripts/price.js
@@ -0,0 +1,6 @@
+function price(value) {
+  let start = document.getElementById("input_start").value;
+  let end = document.getElementsByClassName("input_end").value;
+  console.log(start);
+  document.getElementById("price").innerText = start;
+}
