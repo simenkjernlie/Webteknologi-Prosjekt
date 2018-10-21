dd0ee39b1a4f0c2da95c600cd94274b966423b77
Alexey Gusev <alexeigusev@yahoo.no>
2018-10-22 01:00:26 +0200

    src.

--- src/scripts/price.js
+++ src/scripts/price.js
@@ -1,6 +1,25 @@
-function price(value) {
-  let start = document.getElementById("input_start").value;
-  let end = document.getElementsByClassName("input_end").value;
-  console.log(start);
-  document.getElementById("price").innerText = start;
+function make_price_elements(i, pris) {
+  let img = document.createElement('img');
+  let tall = document.createTextNode(pris);
+  let space = document.createTextNode("\xa0");
+  img.src = "../img/euro-vector.png";
+  img.style.width = "6%";
+  document.getElementsByClassName('price')[i].innerHTML = "";
+  document.getElementsByClassName('price')[i].appendChild(img);
+  document.getElementsByClassName('price')[i].appendChild(space);
+  document.getElementsByClassName('price')[i].appendChild(tall);
+}
+
+function null_ut() {
+  for (i = 0; i < 3; i++) {
+    make_price_elements(i, 0);
+  }
+}
+
+function calc_price(number) {
+  let start = document.getElementsByClassName("input_start")[number].value;
+  let end = document.getElementsByClassName("input_end")[number].value;
+  let x = document.createTextNode(start.split("-"));
+  null_ut();
+  make_price_elements(number, 100.00);
 }
