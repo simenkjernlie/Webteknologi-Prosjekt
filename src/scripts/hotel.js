78a602f9aa1794f047d97b7d6483454faaa62025
Hans Østerdal <osterdalh@gmail.com>
2018-11-02 12:22:43 +0100

    hotels

--- src/scripts/hotel.js
+++ src/scripts/hotel.js
@@ -1,127 +1,86 @@
+document.getElementById("gul_boks1").style.display = "none"
+document.getElementById("gul_boks2").style.display = "none"
+document.getElementById("gul_boks3").style.display = "none"
+document.getElementById("gul_boks4").style.display = "none"
 
 
 
-images = ["../img/hotelbilder/hotel1-1.jpg", "../img/hotelbilder/hotel1-2.jpg", "../img/hotelbilder/hotel1-3.jpeg"]
 
-
-
-function changeImg(){
-    document.getElementById('bilder_slide').src = "../img/hotelbilder/hotel1-1.jpg"
-    console.log(document.getElementById('bilder_slide').src)
-    console.log("heiehi")
-
-}
 
 
 hotel_texts_def = ["Hotel Adriana is one of the best known hotels in the Cagliari area. It is known for its beautiful location by the beach, along with unbeatable service and chefs working around the clock to always offer freash mediterranean courses.", "sdad2", "afa3", "4"]
 hotel_texts_exs = ["Hotel Adriana is one of the best known hotels in the Cagliari area. It is known for its beautiful location by the beach, along with unbeatable service and chefs working around the clock to always offer freash mediterranean courses. <br> <br> The hotel is located in the city center, but the beach is just a 5 minute walk away. Here you can play biljard or hang out in the pool area, while enjoing a feash Mojito from the bar.", "he2", "kl3", "sg4"]
 
+console.log(window.innerWidth)
 
 
-var ut1 = document.getElementById("gul_boks1");
-var img1 = document.getElementById("img_ut1")
-var text1 = document.getElementById('hot_text1')
-var hot1 = document.getElementById('hotel_block1')
 
-ut1.style.display = "none"
-function utvidelse1() {
-    if (ut1.style.display === "none") {
-        ut1.style.display = "block";
-        img1.src = "../img/hotelbilder/arrow.png"
-        hot1.style.height = "700px"
-        text1.innerHTML = hotel_texts_exs[0]
-        img1.style.top = "52%"
-    } else {
-        ut1.style.display = "none";
-        hot1.style.height = "200px"
-        img1.src = "../img/hotelbilder/expand-arrow.png"
-        text1.innerHTML = hotel_texts_def[0]
-        img1.style.top = "80%"
 
+
+function utvidelse(ut_element, parent_element, side, arrow) {
+    let element = document.getElementById(ut_element);
+    let parent = document.getElementById(parent_element);
+    let side_ex = document.getElementById(side);
+    let arrow_ex = document.getElementById(arrow);
+    let width_changer = parent.style.width
+
+    let width = document.body.clientWidth   
+    if (element.style.display === "none") {
+        element.style.display = "block"
+        parent.style.height = "700px"
+        side_ex.style.height = '375px'
+        arrow_ex.src = "../img/hotelbilder/arrow.png"
+        console.log(parent.style.width)
+        arrow_ex.style.top = '375px'
+        //document.getElementById("bilder_slide").style.display = "none"
+        if (window.innerWidth >= 1000){
+            parent.style.height = "700px"
+
+            console.log("over")}
+
+
+   
+
+    } else {        
+            parent.style.height = "200px"
+            element.style.display = "none";
+            side_ex.style.height = '170px'
+            arrow_ex.src = "../img/hotelbilder/expand-arrow.png"
+            arrow_ex.style.top = '80%'
+            //document.getElementById("bilder_slide").style.display = "block"
+
+            if (window.innerWidth < 1000){
+                parent.style.height = "400px"
+
+           // console.log(document.getElementById(body).style.width)
+
+        
     }
-    
-    
+
+}
 }
 
-var ut2 = document.getElementById("gul_boks2");
-var img2 = document.getElementById("img_ut2")
-var text2 = document.getElementById('hot_text2')
-var hot2 = document.getElementById('hotel_block2')
-ut2.style.display = "none"
+var days_in_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+//function nr_days(year, month, day){
+//    var nr_days = (year-1)*365 + (month-1)*days_in_month[]
 
-function utvidelse2() {
-    if (ut2.style.display === "none") {
-        ut2.style.display = "block";
-        hot2.style.height = "700px"
-        img2.style.top = "52%"
-        img2.src = "../img/hotelbilder/arrow.png"
-        ut1.style.display = "none"
-        ut3.style.display = "none"
-        ut4.style.display = "none"
-        hot1.style.height = "200px"
-        hot3.style.height = "200px"
-        hot4.style.height = "200px"
+//}
 
 
+function price(check_in_date, check_out_date, nr_rooms, nr_adults, nr_children){
 
 
+    var check_in_date = document.getElementById("check_in_date").value
+    var check_out_date = document.getElementById("check_out_date").value
+    var nr_rooms = nr_rooms.value
+    var nr_adults = nr_adults.value
+    var nr_children = nr_children.value
+    console.log(check_in_date, check_out_date, nr_rooms, nr_adults, nr_children)
 
-    } else {
-        ut2.style.display = "none";
-        hot2.style.height = "200px"
-        img2.style.top = "80%"
-        img2.src = "../img/hotelbilder/expand-arrow.png"
+    var cid_year1 = check_in_date.slice(0,4)
+    var cid_month1 = check_in_date.slice(5,7)
+    var cid_day1 = check_in_date.slice(8,10)
+    console.log(cid_year1, cid_month1, cid_day1)
 
-    }
+
 }
-
-
-
-var ut3 = document.getElementById("gul_boks3");
-var img3 = document.getElementById("img_ut3")
-var text3 = document.getElementById('hot_text3')
-var hot3 = document.getElementById('hotel_block3')
-ut3.style.display = "none"
-
-function utvidelse3() {
-    if (ut3.style.display === "none") {
-        ut3.style.display = "block";
-        hot3.style.height = "700px"
-        img3.style.top = "52%"
-        img3.src = "../img/hotelbilder/arrow.png"
-
-    } else {
-        ut3.style.display = "none";
-        hot3.style.height = "200px"
-        img3.style.top = "80%"
-        img3.src = "../img/hotelbilder/expand-arrow.png"
-    }
-}
-
-
-var img4 = document.getElementById("img_ut4")
-var text3 = document.getElementById('hot_text4')
-
-
-var ut4 = document.getElementById("gul_boks4");
-var img4 = document.getElementById("img_ut4")
-var text4 = document.getElementById('hot_text4')
-var hot4 = document.getElementById('hotel_block4')
-ut4.style.display = "none"
-
-function utvidelse4() {
-    if (ut4.style.display === "none") {
-        ut4.style.display = "block";
-        hot4.style.height = "700px"
-        img4.style.top = "52%"
-        img4.src = "../img/hotelbilder/arrow.png"
-
-    } else {
-        ut4.style.display = "none";
-        hot4.style.height = "200px"
-        img4.style.top = "80%"
-        img4.src = "../img/hotelbilder/expand-arrow.png"
-    }
-}
-
-
