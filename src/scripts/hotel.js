5c53839efacb386643ba34b0b4b58f30328487c3
Hans Østerdal <osterdalh@gmail.com>
2018-11-09 13:08:39 +0100

    kommentert javascript, hotel

--- src/scripts/hotel.js
+++ src/scripts/hotel.js
@@ -4,22 +4,17 @@ document.getElementById("utvidelse1").style.display = "none"
 document.getElementById("utvidelse2").style.display = "none"
 document.getElementById("utvidelse3").style.display = "none"
 document.getElementById("utvidelse4").style.display = "none"
-
 document.getElementById("linje1").style.display = "none"
 document.getElementById("linje2").style.display = "none"
 document.getElementById("linje3").style.display = "none"
 document.getElementById("linje4").style.display = "none"
 
-
-
-
-
 // funksjon for utvidelse. Tar inn tilhørende utvidelsesblokk-ID (for å vite hvilken blokk som skal utvides), ID for utvidelsespilen (slik
 // at den pathen kan endres slik at den peker opp når blokken er utvided), og ID for linjeutvidelse som også utvides etter blokken
-function utvidelse(utvidelseID, bilde_pil, linje){
+function utvidelse(utvidelseID, bilde_pil, linje) {
     var utvidelse_block = document.getElementById(utvidelseID)
     let bilde_path = document.getElementById(bilde_pil)
-        if (utvidelse_block.style.display == "none"){
+    if (utvidelse_block.style.display == "none") {
         utvidelse_block.style.display = "block"
         bilde_path.src = "../img/arrow-up.svg";
         document.getElementById(linje).style.display = "block"
@@ -39,91 +34,91 @@ function price(check_in_date, check_out_date, nr_rooms, nr_adults, price, price_
     var check_out_date = document.getElementById(check_out_date).value
     var nr_rooms = document.getElementById(nr_rooms).value
     var nr_adults = document.getElementById(nr_adults).value
-    check_in_date = check_in_date.slice(5,7) + "/" + check_in_date.slice(8,10) + "/" + check_in_date.slice(0,4)         // transformere dato til riktig format
-    check_out_date = check_out_date.slice(5,7) + "/" + check_out_date.slice(8,10) + "/" + check_out_date.slice(0,4)
+    check_in_date = check_in_date.slice(5, 7) + "/" + check_in_date.slice(8, 10) + "/" + check_in_date.slice(0, 4)         // transformere dato til riktig format
+    check_out_date = check_out_date.slice(5, 7) + "/" + check_out_date.slice(8, 10) + "/" + check_out_date.slice(0, 4)
     check_in_date = parseDate(check_in_date)
     check_out_date = parseDate(check_out_date)
     days_diff = datediff(check_in_date, check_out_date)
-    var amount = (days_diff*nr_rooms*price_pr_room + days_diff*price_pr_person*nr_adults)
-    if (days_diff > 0){
+    var amount = (days_diff * nr_rooms * price_pr_room + days_diff * price_pr_person * nr_adults)
+    if (days_diff > 0) {
         price.innerHTML = "Book now for only " + amount + "€"
     } else {
         price.innerHTML = "Please fill in form for price estimation"
     }
 }
 
+// funksjon som konverterer datostrenger til riktig format
 function parseDate(str) {
     var mdy = str.split('/');
-    return new Date(mdy[2], mdy[0]-1, mdy[1]);
+    return new Date(mdy[2], mdy[0] - 1, mdy[1]);
 }
 
+// måle forskjellen to datoer og dele på antall millisekunder pr dag, runder av til nærmeste hele tall for å håndtere DST
+// inspirert fra StackOverflow: https://stackoverflow.com/questions/542938/how-do-i-get-the-number-of-days-between-two-dates-in-javascript
 function datediff(first, second) {
-
-    // måle 
-    // Take the difference between the dates and divide by milliseconds per day.
-    // Round to nearest whole number to deal with DST.
-    return Math.round((second-first)/(1000*60*60*24));
+    return Math.round((second - first) / (1000 * 60 * 60 * 24));
 }
 
-function form_validater(check_in_date, check_out_date, hot_name){
+// Tar inn to datoer og sjekker om disse oppfyller kriteriene for å bestille hotell 
+//          - at datoene er i fremtid
+//          - at check in er før check out
+function form_validater(check_in_date, check_out_date, hot_name) {
     var check_in_date = document.getElementById(check_in_date)
     check_in_date = check_in_date.value
     var check_out_date = document.getElementById(check_out_date).value
-    check_in_date = check_in_date.slice(5,7) + "/" + check_in_date.slice(8,10) + "/" + check_in_date.slice(0,4)
-    check_out_date = check_out_date.slice(5,7) + "/" + check_out_date.slice(8,10) + "/" + check_out_date.slice(0,4)
+    check_in_date = check_in_date.slice(5, 7) + "/" + check_in_date.slice(8, 10) + "/" + check_in_date.slice(0, 4)
+    check_out_date = check_out_date.slice(5, 7) + "/" + check_out_date.slice(8, 10) + "/" + check_out_date.slice(0, 4)
 
     var today = new Date();
     var day = today.getDate()
     var month = today.getMonth() + 1
-    var  year = today.getFullYear()
+    var year = today.getFullYear()
     var current_date = month + "/" + day + "/" + year
     current_date = parseDate(current_date)
-    console.log(current_date, check_in_date)
     check_in_date = parseDate(check_in_date)
     var past_diff = datediff(current_date, check_in_date)
     check_out_date = parseDate(check_out_date)
     var days_diff = datediff(check_in_date, check_out_date)
 
-    console.log(past_diff, days_diff)
-    if (past_diff < 0){
+    if (past_diff < 0) {
         alert("Please select valid check in and check out days! \n Select dates in the future")
-        return false        
+        return false
     }
-    else if (days_diff<= 0){
+    else if (days_diff <= 0) {
         alert("Please select valid check in and check out days! \n Check in date before check out day")
         return false;
-    } 
+    }
     else {
         alert("Thank you for your reservation, " + hot_name + " is looking forward for your stay. \n Please check mail for confirmation and receipt")
         return true;
     }
 }
 
-
+// Funksjon som benyttes for å ha interaktiv Google maps API
 var adriana, barbo, marameda, bp;
 function initMap() {
-  adriana = { lat: 39.905319, lng: 8.596118 };
-  barbo = { lat: 40.948178, lng: 9.565120 };
-  marameda = { lat: 39.214894, lng: 9.116970 };
-  bp = { lat: 39.222492, lng: 9.245832 };
-  var mapAdriana = new google.maps.Map(document.getElementById("mapAdriana"), {
-    zoom: 10,
-    center: adriana
-  });
-  var mapBarbo = new google.maps.Map(document.getElementById("mapBarbo"), {
-    zoom: 10,
-    center: barbo
-  });
-  var mapMarameda = new google.maps.Map(document.getElementById("mapMarameda"), {
-    zoom: 10,
-    center: marameda
-  });
-  var mapBP = new google.maps.Map(document.getElementById("mapBP"), {
-    zoom: 10,
-    center: bp
-  });
-  var markerPizza = new google.maps.Marker({ position: adriana, map: mapAdriana });
-  var markerSteak = new google.maps.Marker({ position: barbo, map: mapBarbo });
-  var markerSeafood = new google.maps.Marker({ position: marameda, map: mapMarameda});
-  var markerLobster = new google.maps.Marker({ position: bp, map: mapBP});
+    adriana = { lat: 39.905319, lng: 8.596118 };
+    barbo = { lat: 40.948178, lng: 9.565120 };
+    marameda = { lat: 39.214894, lng: 9.116970 };
+    bp = { lat: 39.222492, lng: 9.245832 };
+    var mapAdriana = new google.maps.Map(document.getElementById("mapAdriana"), {
+        zoom: 10,
+        center: adriana
+    });
+    var mapBarbo = new google.maps.Map(document.getElementById("mapBarbo"), {
+        zoom: 10,
+        center: barbo
+    });
+    var mapMarameda = new google.maps.Map(document.getElementById("mapMarameda"), {
+        zoom: 10,
+        center: marameda
+    });
+    var mapBP = new google.maps.Map(document.getElementById("mapBP"), {
+        zoom: 10,
+        center: bp
+    });
+    var markerPizza = new google.maps.Marker({ position: adriana, map: mapAdriana });
+    var markerSteak = new google.maps.Marker({ position: barbo, map: mapBarbo });
+    var markerSeafood = new google.maps.Marker({ position: marameda, map: mapMarameda });
+    var markerLobster = new google.maps.Marker({ position: bp, map: mapBP });
 }
